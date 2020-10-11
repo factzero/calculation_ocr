4792c38e2092139352269db6226e72fc17ce0d38
factzero <azlinzhi@aliyun.com>
2020-10-11 22:24:35 +0800

    ctpn测试

---
+++ test_ctpn.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import argparse
+import cv2
+import time
+from textdetection.ctpn.ctpn_inference import OcrDetCTPN
+
+
+parser = argparse.ArgumentParser(description='test')
+parser.add_argument('--image_name', default='./data/tx6.jpg', type=str, help='image for processing')
+parser.add_argument('--model_path', default='./checkpoints/CTPN.pth', type=str, help='trained model path')
+
+
+if __name__ == '__main__':
+    opt = parser.parse_args()
+    detect = OcrDetCTPN(opt.model_path)
+    image = cv2.imread(opt.image_name)
+    started = time.time()
+    det_recs, img_framed, image = detect.inference(image)
+    finished = time.time()
+    # for res in det_recs:
+    #     cv2.line(image, (int(res[0]), int(res[1])), (int(res[2]), int(res[3])), (0, 0, 255), 1)
+    #     cv2.line(image, (int(res[0]), int(res[1])), (int(res[4]), int(res[5])), (0, 0, 255), 1)
+    #     cv2.line(image, (int(res[6]), int(res[7])), (int(res[2]), int(res[3])), (0, 0, 255), 1)
+    #     cv2.line(image, (int(res[4]), int(res[5])), (int(res[6]), int(res[7])), (0, 0, 255), 1)
+    cv2.imshow("image", image)
+    cv2.imshow("img_framed", img_framed)
+    cv2.waitKey(0)
+    print('elapsed time: {0}'.format(finished-started))
--- train_ctpn.py
+++ train_ctpn.py
@@ -15,7 +15,6 @@ parser.add_argument('--image_root', default='D:/04download/VOC2007_v0', type=str
 parser.add_argument('--save_folder', default='./checkpoints/', help='Location to save checkpoint models')
 parser.add_argument('--batch_size', default=4, type=int, help='batch size')
 parser.add_argument('--resume_net', default='', help='resume net')
-parser.add_argument('--resume_iter', default=0, type=int, help='resume Iteration')
 
 
 def train(model, loader, criterion_cls, criterion_regr, optimizer, iteration, device):
